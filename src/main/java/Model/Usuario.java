ecca9d8b725a0145628efbd44a5892848050c76f
Cesar Andres Guzman <cesarandresguzman27@gmail.com>
2025-04-03 11:55:12 -0500

    Commit Inicial: Modelo Usuario Finalizado

---
+++ src/main/java/Model/Usuario.java
@@ -0,0 +1,72 @@
+package Model;
+
+import java.util.UUID;
+
+public class Usuario {
+
+    private String id;
+    private String nombre;
+    private String correo;
+    private String telefono;
+    private String usuario;
+    private String contraseña;
+
+    public Usuario() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public Usuario(String nombre, String correo, String telefono, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
